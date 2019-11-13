048f13d97c777bc541ebd58d765317fc4f4a6798
Vinicius Azevedo <viniazvd@gmail.com>
2019-11-13 14:52:23 -0300

    :tada: init

---
+++ src/components/CRange.vue
@@ -0,0 +1,54 @@
+<template>
+  <input
+    step="1"
+    min="100"
+    type="range"
+    class="range"
+    :value="selector"
+    :max="maxRange"
+    @input="range => $emit('update:selector', range)"
+  >
+</template>
+
+<script>
+export default {
+  name: 'c-range',
+
+  props: {
+    selector: Number,
+    maxRange: Number
+  }
+}
+</script>
+
+<style lang="scss">
+.range {
+  height: 8px;
+  width: 180px;
+  margin-top: 40px;
+  -webkit-appearance: none;
+}
+
+.range:focus { outline: none; }
+
+// bar
+.range::-webkit-slider-runnable-track {
+  height: 8px;
+  width: 180px;
+  cursor: pointer;
+  border-radius: 25px;
+  background-color: rgba(#121E48, 0.1);
+}
+
+// ball
+.range::-webkit-slider-thumb {
+  width: 14px;
+  height: 15px;
+  cursor: pointer;
+  margin-top: -4px;
+  border-radius: 15px;
+  background: #FFFFFF;
+  -webkit-appearance: none;
+  box-shadow: 0 0 11px 3px rgba(#121E48, 0.1);
+}
+</style>
---
+++ src/components/CSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <svg
+    v-if="isLoaded && !hasError"
+    fill="white"
+    class="wrapper-selector"
+    :style="{ width: size + 'px', height: size + 'px' }"
+  >
+    <defs>
+      <filter id="shadow">
+        <feDropShadow dx="0" dy="4" stdDeviation="4" flood-opacity=".5" />
+      </filter>
+
+      <clipPath id="mask">
+        <circle :r="selector / 2" :cx="coordinates.x" :cy="coordinates.y" />
+      </clipPath>
+    </defs>
+
+    <circle
+      class="selector"
+      stroke-width="2"
+      stroke="#FFFFFF"
+      filter="url(#shadow)"
+      :r="selector / 2"
+      :cx="coordinates.x"
+      :cy="coordinates.y"
+    />
+
+    <image
+      opacity=".5"
+      preserveAspectRatio="none"
+      style="width: 100%; height: 100%"
+      :href="src"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'c-selector',
+
+  props: {
+    src: String,
+    size: Number,
+    selector: Number,
+    isLoaded: Boolean,
+    hasError: Boolean,
+    coordinates: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.wrapper-selector {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
+}
+</style>
