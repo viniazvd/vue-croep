30c0da41b8d0bd540450f5e7e363443e890b1191
Vinicius Azevedo <viniazvd@gmail.com>
2019-11-13 14:51:02 -0300

    :tada: init

---
+++ examples/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <vue-croep ref="croep" />
+
+    <button @click="crop">crop</button>
+  </div>
+</template>
+
+<script>
+import VueCroep from '../src/Index.vue'
+
+export default {
+  name: 'example',
+
+  components: { VueCroep },
+
+  methods: {
+    crop () {
+      this.$refs.croep.crop()
+    }
+  }
+}
+</script>
