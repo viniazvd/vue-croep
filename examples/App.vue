6ac57cf2585c8bfe504887c6c1bdb3868dce0e76
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-11-19 17:57:44 -0300

    :zap: upgrade example

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <vue-croep ref="croep" />
+    <input type="file" @change="getBase64">
+
+    <vue-croep ref="croep" :src="src" />
 
     <button @click="crop">crop</button>
   </div>
@@ -14,7 +16,21 @@ export default {
 
   components: { VueCroep },
 
+  data () {
+    return {
+      src: ''
+    }
+  },
+
   methods: {
+    getBase64 (event) {
+      const file = event.target.files[0]
+      let reader = new FileReader()
+
+      reader.readAsDataURL(file)
+      reader.onload = () => this.src = reader.result
+    },
+
     crop () {
       this.$refs.croep.crop()
     }
